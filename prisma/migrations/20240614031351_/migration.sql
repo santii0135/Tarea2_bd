32828e53dcacd8de207d2414b8f26246db57b365
santii0135 <santiago.anwandter@usm.cl>
2024-06-18 00:25:58 -0400

    gitignore

--- prisma/migrations/20240614031351_/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Bloquear" DROP CONSTRAINT "Bloquear_blockerEmail_fkey";
-
--- AlterTable
-ALTER TABLE "Bloquear" ALTER COLUMN "blockerEmail" SET DATA TYPE TEXT;
-
--- AddForeignKey
-ALTER TABLE "Bloquear" ADD CONSTRAINT "Bloquear_blockerEmail_fkey" FOREIGN KEY ("blockerEmail") REFERENCES "User"("correo") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20240614040146_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `clave` on the `Bloquear` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Bloquear" DROP COLUMN "clave";
